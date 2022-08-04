a4adb09e3868aadf474f6161a4d45f33aa392a29
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-04 13:50:55 +0545

    feat: add social links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,22 +9,55 @@ const services = [
 	"Transfers",
 ];
 
+const extra = ["Why Innicement", "Fees & commissions", "Benefits", "Account conveniences"];
+
+function FooterLinks() {
+	return (
+		<div className={clsx("flex flex-grow", "gap-2", "justify-between")}>
+			<List className="flex flex-col gap-5">
+				{navItems.map((i) => (
+					<a>{i}</a>
+				))}
+			</List>
+			<List className="flex flex-col gap-5">
+				{services.map((i) => (
+					<a>{i}</a>
+				))}
+			</List>
+			<List className="flex flex-col gap-5">
+				{extra.map((i) => (
+					<a>{i}</a>
+				))}
+			</List>
+			<div></div>
+		</div>
+	);
+}
+
+function Social() {
+	return (
+		<List className="flex gap-3 pt-10">
+			<div className="border-2 rounded-full h-15 w-15" />
+			<div className="border-2 rounded-full h-15 w-15" />
+			<div className="border-2 rounded-full h-15 w-15" />
+			<div className="border-2 rounded-full h-15 w-15" />
+			<div className="border-2 rounded-full h-15 w-15" />
+			<div className="border-2 rounded-full h-15 w-15" />
+		</List>
+	);
+}
+
 export default function Footer() {
 	return (
-		<footer className={clsx("container")}>
-			<div className={clsx("flex", "gap-2", "justify-between", "py-10")}>
+		<footer>
+			<div
+				className={clsx("container", "flex flex-col md:flex-row", "items-start", "py-10", "gap-5")}
+			>
 				<LogoWithText />
-				<List className="flex flex-col gap-5">
-					{navItems.map((i) => (
-						<a>{i}</a>
-					))}
-				</List>
-				<List className="flex flex-col gap-5">
-					{services.map((i) => (
-						<a>{i}</a>
-					))}
-				</List>
-				<div></div>
+				<div className="flex flex-col flex-grow">
+					<FooterLinks />
+					<Social />
+				</div>
 			</div>
 			<h2 className="text-center p-10">© ANP 2022. All rights reserved.</h2>
 		</footer>
