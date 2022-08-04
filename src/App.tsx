3c3f8b07eb33fcc98c75846a8c675ff1d2a315b5
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-04 11:25:12 +0545

    chore: add prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -6,125 +6,111 @@ import Layout from "./components/Layout";
 import { GetStartedButton, NavButton } from "./components/Nav";
 
 function Hero() {
-  return (
-    <section
-      id="hero"
-      className="grid grid min-h-[80vh] grid-cols-1 md:grid-cols-12"
-    >
-      <div
-        className={clsx(
-          "col-span-6",
-          "flex flex-col gap-8 justify-evenly items-start"
-        )}
-      >
-        <img className="h-[20%]" src={"/get_upto_80k.svg"} />
-        <h1 className="hero-title *header-1">
-          Do more with investments on your own terms
-        </h1>
-        <GetStartedButton className="border-0 w-full md:w-1/3 " />
-      </div>
-      <div
-        className={clsx("col-span-6", "grid grid-cols-2 grid-rows-1", "gap-8")}
-      >
-        <div className="flex flex-col col-span-1 gap-12">
-          <img className="flex-grow" src="http://source.unsplash.com/random" />
-          <p>
-            Get helpful investing ideas and a cash bonus when you invest in a
-            new account with Innicement
-          </p>
-        </div>
-        <div className="flex flex-col">
-          <p className="bg-black">
-            Get helpful investing ideas and a cash bonus when you invest in a
-            new account with Innicement
-          </p>
-          <img className="flex-grow" src="http://source.unsplash.com/random" />
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section id="hero" className="grid grid min-h-[80vh] grid-cols-1 md:grid-cols-12">
+			<div className={clsx("col-span-6", "flex flex-col gap-8 justify-evenly items-start")}>
+				<img className="h-[20%]" src={"/get_upto_80k.svg"} />
+				<h1 className="hero-title *header-1">Do more with investments on your own terms</h1>
+				<GetStartedButton className="border-0 w-full md:w-1/3 " />
+			</div>
+			<div className={clsx("col-span-6", "grid grid-cols-2 grid-rows-1", "gap-8")}>
+				<div className="flex flex-col col-span-1 gap-12">
+					<img className="flex-grow" src="http://source.unsplash.com/random" />
+					<p>
+						Get helpful investing ideas and a cash bonus when you invest in a new account with
+						Innicement
+					</p>
+				</div>
+				<div className="flex flex-col">
+					<p className="bg-black">
+						Get helpful investing ideas and a cash bonus when you invest in a new account with
+						Innicement
+					</p>
+					<img className="flex-grow" src="http://source.unsplash.com/random" />
+				</div>
+			</div>
+		</section>
+	);
 }
 
-function Summary({
-  title,
-  children,
-}: React.ComponentPropsWithoutRef<"details">) {
-  return (
-    <details className="">
-      <summary className="cursor-pointer text-3xl">{title}</summary>
-      {children}
-    </details>
-  );
+function Summary({ title, children }: React.ComponentPropsWithoutRef<"details">) {
+	return (
+		<details className="">
+			<summary className="cursor-pointer text-3xl">{title}</summary>
+			{children}
+		</details>
+	);
 }
 
 function Info() {
-  return (
-    <section id="info" className="min-h-screen grid grid-cols-12 gap-8">
-      <div className="col-span-3">
-        <img src="http://source.unsplash.com/random" />
-      </div>
-      <div className="col-span-6">
-        <SubHeading>Everything you need to invest the way you want</SubHeading>
-        <Summary title="Investing ideas">
-          <p>
-            Innicement has the guidance and tools to confidently pursue your
-            investing goals and build your portfolio.
-          </p>
-        </Summary>
-        <Summary title="$0 trades">lorem</Summary>
-        <Summary title="More rewards">lorem</Summary>
-        <Summary title="Investing & connected">lorem</Summary>
-      </div>
-      <div className="col-span-3"></div>
-    </section>
-  );
+	return (
+		<section id="info" className="min-h-screen grid grid-cols-12 gap-8">
+			<div className="col-span-3">
+				<img src="http://source.unsplash.com/random" />
+			</div>
+			<div className="col-span-6">
+				<SubHeading>Everything you need to invest the way you want</SubHeading>
+				<Summary title="Investing ideas">
+					<p>
+						Innicement has the guidance and tools to confidently pursue your investing goals and
+						build your portfolio.
+					</p>
+				</Summary>
+				<Summary title="$0 trades">lorem</Summary>
+				<Summary title="More rewards">lorem</Summary>
+				<Summary title="Investing & connected">lorem</Summary>
+			</div>
+			<div className="col-span-3"></div>
+		</section>
+	);
 }
 
 function Pricing() {
-  return (
-    <section id="pricing" className="min-h-screen">
-      <SubHeading>Invest your way</SubHeading>
-    </section>
-  );
+	return (
+		<section id="pricing" className="min-h-screen">
+			<div>
+				<SubHeading>Invest your way</SubHeading>
+				<NavButton>See Details</NavButton>
+			</div>
+		</section>
+	);
 }
 
 function App() {
-  return (
-    <Layout className="container flex flex-col gap-10">
-      <Hero />
-      <Info />
-      <Pricing />
-      <section className="min-h-screen">
-        <SubHeading>Lower costs can add up to big savings</SubHeading>
-      </section>
-      <section className="min-h-screen">
-        <div>
-          <SubHeading>Innicement Digital Advisor</SubHeading>
-          <p>
-            For those who prefer the convenience of a robo-advisor, Innicement
-            Digital Advisor will build a customized portfolio and continue to
-            optimize your investments over time. You get the benefit of
-            Innicement time-tested philosophy delivered on a modern, all-digital
-            platform that can evolve with your needs.
-          </p>
-          <NavButton>Learn More</NavButton>
-        </div>
-      </section>
-      <section className="min-h-screen">
-        <SubHeading>Preferred Rewards</SubHeading>
-        <p>
-          Investment balances can help you earn Preferred Rewards benefits and
-          pricing discounts. Members enjoy program benefits including credit
-          card rewards bonuses, discounts on home and auto loans, monthly
-          account maintenance fee waivers, and more.
-          <button>Learn More</button>
-        </p>
-      </section>
-      <section className="get-started min-h-screen">
-        <SubHeading>Get up to when you invest in Innicement</SubHeading>
-      </section>
-    </Layout>
-  );
+	return (
+		<Layout className="container flex flex-col gap-10">
+			<Hero />
+			<Info />
+			<Pricing />
+			<section className="min-h-screen">
+				<SubHeading>Lower costs can add up to big savings</SubHeading>
+			</section>
+			<section className="min-h-screen">
+				<div>
+					<SubHeading>Innicement Digital Advisor</SubHeading>
+					<p>
+						For those who prefer the convenience of a robo-advisor, Innicement Digital Advisor will
+						build a customized portfolio and continue to optimize your investments over time. You
+						get the benefit of Innicement time-tested philosophy delivered on a modern, all-digital
+						platform that can evolve with your needs.
+					</p>
+					<NavButton>Learn More</NavButton>
+				</div>
+			</section>
+			<section className="min-h-screen">
+				<SubHeading>Preferred Rewards</SubHeading>
+				<p>
+					Investment balances can help you earn Preferred Rewards benefits and pricing discounts.
+					Members enjoy program benefits including credit card rewards bonuses, discounts on home
+					and auto loans, monthly account maintenance fee waivers, and more.
+					<button>Learn More</button>
+				</p>
+			</section>
+			<section className="get-started min-h-screen">
+				<SubHeading>Get up to when you invest in Innicement</SubHeading>
+			</section>
+		</Layout>
+	);
 }
 
 export default App;
