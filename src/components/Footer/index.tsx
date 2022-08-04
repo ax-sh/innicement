5f15918029a19adb816e6d500222a11ab80d1127
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-04 13:39:43 +0545

    feat: add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,32 @@
 import clsx from "clsx";
+import { List, LogoWithText, navItems } from "..";
+
+const services = [
+	"Compare Services",
+	"Investment Products",
+	"Account & Plans",
+	"Rollovers",
+	"Transfers",
+];
 
 export default function Footer() {
-  return (
-    <footer
-      className={clsx("container", "flex", "gap-2", "py-10", "justify-between")}
-    ></footer>
-  );
+	return (
+		<footer className={clsx("container")}>
+			<div className={clsx("flex", "gap-2", "justify-between", "py-10")}>
+				<LogoWithText />
+				<List className="flex flex-col gap-5">
+					{navItems.map((i) => (
+						<a>{i}</a>
+					))}
+				</List>
+				<List className="flex flex-col gap-5">
+					{services.map((i) => (
+						<a>{i}</a>
+					))}
+				</List>
+				<div></div>
+			</div>
+			<h2 className="text-center p-10">© ANP 2022. All rights reserved.</h2>
+		</footer>
+	);
 }
