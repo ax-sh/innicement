d14af48b05424cdb50a9116a2a0a8f02bf59bb2a
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-04 14:19:53 +0545

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [WindiCSS(), react()],
+	base: "https://ax-sh.github.io/innicement/",
+	plugins: [WindiCSS(), react()],
 });
