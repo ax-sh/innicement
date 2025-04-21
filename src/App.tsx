637127fc48e5551ae64f1894a0f72003d5fc8136
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-21 16:24:19 +0545

    feat: add Layout component to Loader function

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Suspense } from 'react';
 import { useRoutes } from 'react-router';
 import routes from '~react-pages';
 import { FillViewPort } from './components/fill-view-port';
+import Layout from "./components/Layout";
 
 function Loader() {
   return (
+		<Layout>
+
     <FillViewPort>
       <h1>Innicement</h1>
     </FillViewPort>
+		</Layout>
   );
 }
 function App() {
