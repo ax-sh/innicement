bfc32a2cc6fa648d0b6a19f4c317d4a1a666de67
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-21 15:43:28 +0545

    Updates

---
+++ assets/_...all_-Bcl7naG1.js
@@ -0,0 +1,1 @@
+import{j as r,F as e}from"./index-jH9lftbn.js";function t(){return r.jsx(e,{className:"bg-red-500",children:"404"})}export{t as default};
--- assets/_...all_-DTW-F1ih.js
+++
@@ -1,1 +0,0 @@
-import{j as r,F as e}from"./index-BWgM3pwC.js";function t(){return r.jsx(e,{className:"bg-red-500",children:"404"})}export{t as default};
